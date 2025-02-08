13e1cdb1697874b9c49053ebdbf1e4a9ea70a5f2
is-tchrt <git@is.tchrt.com>
2025-02-07 18:07:11 -0700

    Update copy constructor for ChessBoard to do a deep copy

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -19,7 +19,12 @@ public class ChessBoard {
     }
 
     public ChessBoard(ChessBoard toCopy) {
-        board = toCopy.board;
+        board = new ChessPiece[8][8];
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                board[i][j] = new ChessPiece(toCopy.board[i][j]);
+            }
+        }
     }
 
     /**
