b0f74bcf0588277c28bae744a9de0e32ff511ae5
is-tchrt <git@is.tchrt.com>
2025-02-26 00:18:39 -0700

    Add test for 400 error for register

--- server/src/test/java/ServiceTests.java
+++ server/src/test/java/ServiceTests.java
@@ -3,13 +3,27 @@ import dataaccess.*;
 import model.AuthData;
 import model.GameData;
 import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import requestResult.RegisterRequest;
 import requestResult.RegisterResult;
 import service.Service;
 import service.UserService;
 
+import java.beans.beancontext.BeanContextChild;
+
 public class ServiceTests {
+    UserDao users = new MemoryUserDao();
+    GameDao games = new MemoryGameDao();
+    AuthDao tokens = new MemoryAuthDao();
+
+    UserService service = new UserService(users, games, tokens);
+
+    @BeforeEach
+    void reset() {
+        service.clear();
+    }
+
     @Test
     void clear() throws DataAccessException {
         UserDao users = new MemoryUserDao();
@@ -30,12 +44,12 @@ public class ServiceTests {
     }
 
     @Test
-    void register() throws DataAccessException {
-        UserDao users = new MemoryUserDao();
-        GameDao games = new MemoryGameDao();
-        AuthDao tokens = new MemoryAuthDao();
-
-        UserService service = new UserService(users, games, tokens);
+    void register_success() throws DataAccessException {
+//        UserDao users = new MemoryUserDao();
+//        GameDao games = new MemoryGameDao();
+//        AuthDao tokens = new MemoryAuthDao();
+//
+//        UserService service = new UserService(users, games, tokens);
         RegisterRequest request = new RegisterRequest("isaac", "password", "email");
 
         RegisterResult result = service.register(request);
@@ -43,5 +57,14 @@ public class ServiceTests {
         assert !users.listUsers().isEmpty();
         assert !tokens.listAuthTokens().isEmpty();
         assert result.userName().equals("isaac");
+    }
+
+    @Test
+    void register_400() throws DataAccessException {
+        RegisterRequest request = new RegisterRequest("", "password", "email");
+
+        RegisterResult result = service.register(request);
+
+        assert result.message().equals("Error: bad request");
     }
 }
