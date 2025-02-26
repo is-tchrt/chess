ea3d2141397383e991c10acf3a8f48c89049f1ea
is-tchrt <git@is.tchrt.com>
2025-02-25 23:30:18 -0700

    Improve test for clear() service call

--- server/src/test/java/ServiceTests.java
+++ server/src/test/java/ServiceTests.java
@@ -4,10 +4,9 @@ import model.AuthData;
 import model.GameData;
 import model.UserData;
 import org.junit.jupiter.api.Test;
+import service.Service;
 
 public class ServiceTests {
-    final Service service = new Service();
-
     @Test
     void clear() throws DataAccessException {
         UserDao users = new MemoryUserDao();
@@ -17,6 +16,8 @@ public class ServiceTests {
         users.addUser(new UserData("isaac", "isaac", "isaac"));
         games.addGame(new GameData(1, "white", "black", "game", new ChessGame()));
         tokens.addAuthToken(new AuthData("authToken", "isaac"));
+
+        Service service = new Service(users, games, tokens);
 
         service.clear();
 
