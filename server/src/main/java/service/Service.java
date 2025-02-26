8ab91a7721516b27d188ab55e687148e0e88c9dc
is-tchrt <git@is.tchrt.com>
2025-02-25 23:30:30 -0700

    Create Service class

---
+++ server/src/main/java/service/Service.java
@@ -0,0 +1,22 @@
+package service;
+
+import dataaccess.AuthDao;
+import dataaccess.GameDao;
+import dataaccess.UserDao;
+import org.eclipse.jetty.server.Authentication;
+
+public class Service {
+    private UserDao users;
+    private GameDao games;
+    private AuthDao tokens;
+
+    public Service(UserDao users, GameDao games, AuthDao tokens) {
+        this.users = users;
+        this.games = games;
+        this.tokens = tokens;
+    }
+
+    public void clear() {
+        throw new RuntimeException();
+    }
+}
