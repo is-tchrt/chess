3bfd4a9fb8824af38f70c368b21b8e7c685206d1
is-tchrt <git@is.tchrt.com>
2025-03-08 13:00:09 -0700

    Pass addGame test

--- server/src/main/java/dataaccess/GameDao.java
+++ server/src/main/java/dataaccess/GameDao.java
@@ -6,7 +6,7 @@ import java.util.Collection;
 
 public interface GameDao {
     public void clearGames();
-    public void addGame(GameData game);
+    public void addGame(GameData game) throws DataAccessException;
     public Collection<GameData> listGames();
     public GameData getGame(int gameID);
 }
--- server/src/main/java/dataaccess/MySqlGameDao.java
+++ server/src/main/java/dataaccess/MySqlGameDao.java
@@ -1,19 +1,42 @@
 package dataaccess;
 
+import com.google.gson.Gson;
 import model.GameData;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
 
 public class MySqlGameDao implements GameDao {
+
+    public MySqlGameDao() throws DataAccessException {
+        configureDatabase();
+    }
+
     @Override
     public void clearGames() {
         throw new RuntimeException("Not implemented");
     }
 
     @Override
-    public void addGame(GameData game) {
-        throw new RuntimeException("Not implemented");
+    public void addGame(GameData game) throws DataAccessException {
+        String statement =
+                "INSERT INTO games (gameID, whiteUsername, blackUsername, gameName, game) VALUES (?, ?, ?, ?, ?)";
+        String gameDataJson = new Gson().toJson(game.game());
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.setInt(1, game.gameID());
+                preparedStatement.setString(2, game.whiteUsername());
+                preparedStatement.setString(3, game.blackUsername());
+                preparedStatement.setString(4, game.blackUsername());
+                preparedStatement.setString(5, gameDataJson);
+                preparedStatement.executeUpdate();
+            }
+        } catch (Exception e) {
+            throw new DataAccessException("Error updating database: ".concat(e.getMessage()));
+        }
     }
 
     @Override
@@ -24,5 +47,35 @@ public class MySqlGameDao implements GameDao {
     @Override
     public GameData getGame(int gameID) {
         throw new RuntimeException("Not implemented");
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS games (
+                `gameID` int NOT NULL,
+                `whiteUsername` varchar(256),
+                `blackUsername` varchar(256),
+                `gameName` varchar(256),
+                `game` TEXT,
+                PRIMARY KEY (`gameID`)
+            );
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        try {
+            DatabaseManager.createDatabase();
+        } catch(DataAccessException e) {
+            throw new RuntimeException(String.format("Error creating database: %s", e.getMessage()));
+        }
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException("Error configuring database: ".concat(e.getMessage()));
+        }
     }
 }
