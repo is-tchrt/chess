2e448297a3a6132f3df601453a0aa82c9e4ef37d
is-tchrt <git@is.tchrt.com>
2025-03-07 22:56:55 -0700

    Add addGame test

--- server/src/test/java/dataaccess/GameDaoTests.java
+++ server/src/test/java/dataaccess/GameDaoTests.java
@@ -1,4 +1,18 @@
 package dataaccess;
 
+import chess.ChessGame;
+import model.GameData;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 public class GameDaoTests {
+    GameDao db = new MySqlGameDao();
+
+    @Test
+    public void addGame() {
+        GameData game = new GameData(1, null, null, "gameName", new ChessGame());
+
+        assertDoesNotThrow(() -> db.addGame(game));
+    }
 }
