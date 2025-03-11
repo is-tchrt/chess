4aeaea24ce70de3d78ab56638c6de90ac51b8537
is-tchrt <git@is.tchrt.com>
2025-03-10 22:25:59 -0600

    Add list users test

--- server/src/test/java/dataaccess/UserDaoTests.java
+++ server/src/test/java/dataaccess/UserDaoTests.java
@@ -4,7 +4,9 @@ import model.AuthData;
 import model.UserData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mindrot.jbcrypt.BCrypt;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -47,5 +49,34 @@ public class UserDaoTests {
         users.addUser(user);
         users.clearUsers();
         assertDoesNotThrow(() -> users.addUser(user));
+    }
+
+    @Test
+    public void listUsers() throws DataAccessException {
+        ArrayList<UserData> expected = new ArrayList<>();
+        UserData user1 =
+                new UserData("name1", "password1", "email1");
+        UserData user2 =
+                new UserData("name2", "password2", "email2");
+        UserData user3 =
+                new UserData("name3", "password3", "email3");
+
+        users.addUser(user1);
+        users.addUser(user2);
+        users.addUser(user3);
+
+        user1 =
+                new UserData("name1", BCrypt.hashpw("password1", BCrypt.gensalt()), "email1");
+        user2 =
+                new UserData("name2", BCrypt.hashpw("password2", BCrypt.gensalt()), "email2");
+        user3 =
+                new UserData("name3", BCrypt.hashpw("password3", BCrypt.gensalt()), "email3");
+
+        expected.add(user1);
+        expected.add(user2);
+        expected.add(user3);
+
+        ArrayList<UserData> actual = new ArrayList<>(users.listUsers());
+        assert expected.equals(actual);
     }
 }
