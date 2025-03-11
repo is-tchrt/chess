c275d0a53f438552bf0a9ae08ca285eb6fe75e57
is-tchrt <git@is.tchrt.com>
2025-03-10 18:38:30 -0600

    Add listGames test

--- server/src/test/java/dataaccess/GameDaoTests.java
+++ server/src/test/java/dataaccess/GameDaoTests.java
@@ -5,6 +5,9 @@ import model.GameData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -45,5 +48,26 @@ public class GameDaoTests {
         games.addGame(game);
         games.clearGames();
         assertDoesNotThrow(() -> games.addGame(game));
+    }
+
+    @Test
+    public void listGames() throws DataAccessException {
+        ArrayList<GameData> expected = new ArrayList<>();
+        GameData game1 = new GameData(1, null, null, "game1", new ChessGame());
+        GameData game2 = new GameData(2, "white", null, "game2", new ChessGame());
+        GameData game3 = new GameData(3, null, "black", "game3", new ChessGame());
+
+        expected.add(game1);
+        expected.add(game2);
+        expected.add(game3);
+
+        for (GameData game : expected) {
+            games.addGame(game);
+        }
+
+        ArrayList<GameData> actual = new ArrayList<>(games.listGames());
+        System.out.println(expected);
+        System.out.println(actual);
+        assert expected.equals(actual);
     }
 }
