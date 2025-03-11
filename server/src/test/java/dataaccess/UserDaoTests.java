c2fad4e26857de35a17c10b7f27fa9375ea8d86d
is-tchrt <git@is.tchrt.com>
2025-03-10 22:17:35 -0600

    Add negative addUser test and clear user test

--- server/src/test/java/dataaccess/UserDaoTests.java
+++ server/src/test/java/dataaccess/UserDaoTests.java
@@ -1,8 +1,11 @@
 package dataaccess;
 
+import model.AuthData;
 import model.UserData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
@@ -19,6 +22,30 @@ public class UserDaoTests {
     public void addUser() {
         UserData user = new UserData("name", "password", "email");
 
+        assertDoesNotThrow(() -> users.addUser(user));
+    }
+
+    @Test
+    public void addUsersDuplicateUsername() throws DataAccessException {
+        UserData user = new UserData("name", "password", "email");
+        UserData user2 = new UserData("name", "password", "email");
+
+        users.addUser(user);
+
+        String error = "";
+        try {
+            users.addUser(user2);
+        } catch (Exception e) {
+            error = e.getMessage();
+        }
+        assert !error.isBlank();
+    }
+
+    @Test
+    public void clearUsers() throws DataAccessException {
+        UserData user = new UserData("name", "password", "email");
+        users.addUser(user);
+        users.clearUsers();
         assertDoesNotThrow(() -> users.addUser(user));
     }
 }
