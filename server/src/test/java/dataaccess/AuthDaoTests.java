405368c5d0d61cbf5a6eb3f21fbb52c932206817
is-tchrt <git@is.tchrt.com>
2025-03-10 20:55:35 -0600

    Rename addGame test to addAuthToken test

--- server/src/test/java/dataaccess/AuthDaoTests.java
+++ server/src/test/java/dataaccess/AuthDaoTests.java
@@ -1,8 +1,6 @@
 package dataaccess;
 
-import chess.ChessGame;
 import model.AuthData;
-import model.GameData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,11 +14,10 @@ public class AuthDaoTests {
     @BeforeEach
     public void initializeDatabase() throws DataAccessException {
         tokens = new MySqlAuthDao();
-        tokens.clearAuthTokens();
     }
 
     @Test
-    public void addGame() {
+    public void addAuthToken() {
         AuthData token = new AuthData(UUID.randomUUID().toString(), "name");
 
         assertDoesNotThrow(() -> tokens.addAuthToken(token));
