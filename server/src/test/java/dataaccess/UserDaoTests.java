094d11c998ef810223b390307146ddd509b65b0f
is-tchrt <git@is.tchrt.com>
2025-03-10 21:59:55 -0600

    Add addUser test

--- server/src/test/java/dataaccess/UserDaoTests.java
+++ server/src/test/java/dataaccess/UserDaoTests.java
@@ -1,4 +1,24 @@
 package dataaccess;
 
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 public class UserDaoTests {
+    UserDao users;
+
+    @BeforeEach
+    public void initializeDatabase() throws DataAccessException {
+        users = new MySqlUserDao();
+//        tokens.clearAuthTokens();
+    }
+
+    @Test
+    public void addUser() {
+        UserData user = new UserData("name", "password", "email");
+
+        assertDoesNotThrow(() -> users.addUser(user));
+    }
 }
