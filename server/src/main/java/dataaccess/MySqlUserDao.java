cbe8f82494004ec5a42b614399c4c7429d2be25a
is-tchrt <git@is.tchrt.com>
2025-03-10 22:11:57 -0600

    Pass addUser test

--- server/src/main/java/dataaccess/MySqlUserDao.java
+++ server/src/main/java/dataaccess/MySqlUserDao.java
@@ -1,19 +1,31 @@
 package dataaccess;
 
 import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Collection;
-import java.util.List;
+
+import static java.sql.Types.NULL;
 
 public class MySqlUserDao implements UserDao {
+
+    public MySqlUserDao() throws DataAccessException {
+        configureDatabase();
+    }
+
     @Override
     public void clearUsers() {
         throw new RuntimeException("Not implemented");
     }
 
     @Override
-    public void addUser(UserData user) {
-        throw new RuntimeException("Not implemented");
+    public void addUser(UserData user) throws DataAccessException {
+        String statement = "INSERT INTO users (username, password, email) VALUES (?, ?, ?);";
+        String hashedPassword = BCrypt.hashpw(user.password(), BCrypt.gensalt());
+        executeUpdate(statement, user.username(), hashedPassword, user.email());
     }
 
     @Override
@@ -34,5 +46,62 @@ public class MySqlUserDao implements UserDao {
     @Override
     public void removeUser(String username) {
         throw new RuntimeException("Not implemented");
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                `id` int NOT NULL AUTO_INCREMENT,
+                `username` varchar(256),
+                `password` varchar(256),
+                `email` varchar(256),
+                PRIMARY KEY (`id`),
+                INDEX(username),
+                INDEX(password),
+                UNIQUE(username)
+            );
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        try {
+            DatabaseManager.createDatabase();
+        } catch(DataAccessException e) {
+            throw new RuntimeException(String.format("Error creating database: %s", e.getMessage()));
+        }
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException("Error configuring database: ".concat(e.getMessage()));
+        }
+    }
+
+    private void executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                PreparedStatement updatedStatement = setStatementVariables(ps, params);
+                updatedStatement.executeUpdate();
+            }
+        } catch (Exception e) {
+            throw new DataAccessException("Error updating database: ".concat(e.getMessage()));
+        }
+    }
+
+    private PreparedStatement setStatementVariables(PreparedStatement ps, Object ... params) throws SQLException {
+        for (int i = 0; i < params.length; i++) {
+            var param = params[i];
+            switch (param) {
+                case Integer p -> ps.setInt(i + 1, p);
+                case String p -> ps.setString(i + 1, p);
+                case null -> ps.setNull(i + 1, NULL);
+                default -> {
+                }
+            }
+        }
+        return ps;
     }
 }
