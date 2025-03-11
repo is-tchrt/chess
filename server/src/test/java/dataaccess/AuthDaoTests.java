daa3c677379a444bfecaefbce5b25eb7917f6ec5
is-tchrt <git@is.tchrt.com>
2025-03-10 20:08:46 -0600

    Add addGame test

--- server/src/test/java/dataaccess/AuthDaoTests.java
+++ server/src/test/java/dataaccess/AuthDaoTests.java
@@ -1,4 +1,28 @@
 package dataaccess;
 
+import chess.ChessGame;
+import model.AuthData;
+import model.GameData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 public class AuthDaoTests {
+    AuthDao tokens;
+
+    @BeforeEach
+    public void initializeDatabase() throws DataAccessException {
+        tokens = new MySqlAuthDao();
+        tokens.clearAuthTokens();
+    }
+
+    @Test
+    public void addGame() {
+        AuthData token = new AuthData(UUID.randomUUID().toString(), "name");
+
+        assertDoesNotThrow(() -> tokens.addAuthToken(token));
+    }
 }
