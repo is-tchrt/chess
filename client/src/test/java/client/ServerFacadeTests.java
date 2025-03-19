544e84337091e0d6b524508745ceebe029a58d7b
is-tchrt <git@is.tchrt.com>
2025-03-19 15:56:51 -0600

    Add register test

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,5 +1,7 @@
 package client;
 
+import model.AuthData;
+import model.UserData;
 import org.junit.jupiter.api.*;
 import server.Server;
 
@@ -27,4 +29,25 @@ public class ServerFacadeTests {
     public void sampleTest() {
         Assertions.assertTrue(true);
     }
+
+    @Test
+    public void register() {
+        UserData user = new UserData("user", "password", "email");
+        AuthData result = serverFacade.register(user);
+
+        assert result.username().equals("user");
+        assert !result.authToken().isBlank();
+    }
+//
+//    public void registerDuplicate() {
+//        UserData user = new UserData("user", "password", "email");
+//        UserData user2 = new UserData("user", "password", "email");
+//
+//        serverFacade.register(user);
+//        try {
+//            AuthData result = serverFacade.register(user2);
+//        } catch (Exception e) {
+//            assert e.getMessage().equals("Error: already taken");
+//        }
+//    }
 }
