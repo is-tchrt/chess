e195b65f7661c6eb33d4d7603d8b5d5d87eaa690
is-tchrt <git@is.tchrt.com>
2025-03-19 15:41:49 -0600

    Add function signatures for all public ServerFacade methods

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,7 +1,41 @@
 package client;
 
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.Collection;
+
 public class ServerFacade {
     public ServerFacade(String url) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public void clear() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public AuthData register(UserData user) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public AuthData login(String username, String password) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public void logout(String authToken) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public int createGame(String gameName) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public Collection<GameData> listGames(String authToken) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public void joinGame(String playerColor, int gameID) {
         throw new RuntimeException("Not implemented");
     }
 }
